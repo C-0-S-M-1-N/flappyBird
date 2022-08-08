c3db4495a327499083aef3ab14ea026a5ddc3375
C0sM1n <bordeic4@gmail.com>
2022-08-08 17:05:59 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,109 @@
+#define SFML_STATIC
+#include <bits/stdc++.h>
+#include "SFML/Graphics.hpp"
+#include "player.hpp"
+#define MIN 80
+#define MAX win.getSize().y-80*4
+
+bool gameOver = 0;
+int score = 0;
+Player player(512/4, 20);
+Pipe _pipe_;
+sf::Sprite bkg;
+sf::Texture t, b, ti, td, pipeU, pipeD;
+std::vector<int> pipePos;
+std::vector<std::pair<float, float>> pos;
+sf::Font font;
+
+void draw(sf::RenderWindow &win, sf::Time time){
+	player.draw(win, time);
+	// _pipe_.draw(win);
+}
+void logic(sf::RenderWindow &win){
+	if(pos.empty()){
+		pos.push_back(std::make_pair(win.getSize().x, rand()%(MAX-MIN)+MIN));
+	}
+	for(int i = 0; i<pos.size(); i++){
+		pos[i].first -= 1.5f;
+
+		_pipe_.setPos(pos[i].first, pos[i].second);
+		
+		if(player.colision(_pipe_.getD()) || player.colision(_pipe_.getU())){
+			std::cout<<"Game Over"<<std::endl;
+			gameOver = 1;
+		}
+		if(pos[i].first == 512/4) score++;
+		_pipe_.draw(win);
+	}
+	if(pos[0].first <= -30) pos.erase(pos.begin());
+	if((int)pos[pos.size()-1].first == win.getSize().x - 270){
+		pos.push_back(std::make_pair(win.getSize().x + 30, rand()%(MAX-MIN)+MIN));
+	}
+}
+
+auto main() -> int{
+	srand(unsigned(time(NULL)));
+	sf::RenderWindow win(sf::VideoMode(512, 768), "Flappy Bird");
+	sf::Event _e;
+	sf::Clock c;
+	sf::Time time;
+
+	b.loadFromFile("../src/sprite.png", sf::IntRect(0, 0, 128, 256));
+	bkg.setTexture(b);
+	bkg.setScale(4, 3);
+	
+	t.loadFromFile("../src/sprite.png", sf::IntRect(0, 490, 24, 24));
+	ti.loadFromFile("../src/sprite.png", sf::IntRect(24, 490, 24, 24));
+	td.loadFromFile("../src/sprite.png", sf::IntRect(56, 490, 24, 24));
+	pipeD.loadFromFile("../src/sprite.png", sf::IntRect(55, 325, 30, 160));
+	pipeU.loadFromFile("../src/sprite.png", sf::IntRect(82, 323, 30, 160));
+
+	font.loadFromFile("../src/font.ttf");
+	sf::Text scoreText;
+	scoreText.setFont(font);
+	scoreText.setString(std::to_string(score));
+	scoreText.setCharacterSize(50);
+	scoreText.setOrigin(scoreText.getGlobalBounds().width/2, scoreText.getGlobalBounds().height/2);
+	scoreText.setPosition(win.getSize().x/2, win.getSize().y/2);
+
+	player.setTexture(ti, t, td);
+	_pipe_.setTexture(pipeU, pipeD);
+	// min = 80
+	// max = win.getSize().y - 80*4
+	// _pipe_.setPos(512/4, win.getSize().y-80*4);
+
+	win.setVerticalSyncEnabled(1);
+	time = c.getElapsedTime();
+	while(win.isOpen()){
+		win.clear();
+		win.draw(bkg);
+		
+		/*Handle Window events (closing and space input)*/
+		while(win.pollEvent(_e)){
+			switch(_e.type){
+				case sf::Event::Closed:
+					win.close();
+					break;
+				case sf::Event::KeyReleased:
+					if(_e.key.code == sf::Keyboard::Space) player.addForce(7000);
+					break;
+			}
+		}
+
+		/*accual code*/
+		if(!gameOver){
+			logic(win);
+			time = c.getElapsedTime();
+			c.restart();
+			draw(win, time);
+
+			scoreText.setString(std::to_string(score));
+			scoreText.setOrigin(scoreText.getGlobalBounds().width/2, scoreText.getGlobalBounds().height/2);
+			scoreText.setPosition(win.getSize().x/2, 0+scoreText.getGlobalBounds().height);
+			win.draw(scoreText);
+
+
+			win.display();
+		}
+	}
+}
---
+++ src/player.hpp
@@ -0,0 +1,88 @@
+#pragma once
+#include"SFML/Graphics.hpp"
+#include<iostream>
+
+#define gVector 9.8
+
+extern bool gameOver;
+
+class Player{
+	float Y; //height
+	const int x, mass;
+	sf::Texture idle, up, down;
+	sf::RectangleShape body;
+	float speed = 0, previousSpeed = 0, force = 0, acc = 0;
+
+public:
+	Player(int _x, int m): x{_x}, mass{m} {
+		Y = 180.0f;
+		force = gVector / (mass*1.0f);
+	}
+	void addForce(float f){
+		speed = 0;
+		acc = f / mass;
+	}
+
+	void draw(sf::RenderWindow &win, sf::Time t){
+		if(Y <= body.getSize().y || Y >= win.getSize().y - body.getSize().y) {addForce(0); gameOver = true;}
+		// std::cout<<"speed: "<<speed<<" acc: "<<acc<<" force: "<<force<<" Y: "<<Y<<std::endl;
+		speed += acc;
+		Y -= speed*t.asSeconds();
+		acc = -gVector;
+		// force = 0;
+		if(speed > 0) {
+			body.setTexture(&down);	
+		}
+		else if(speed < 0) {
+			body.setTexture(&up);
+		}
+		else body.setTexture(&idle);
+
+		body.setPosition(x, Y);
+		win.draw(body);
+		
+	}
+	void setTexture(sf::Texture &id, sf::Texture &u, sf::Texture &dw){
+		idle = id;
+		up = u;
+		down = dw;
+		
+		body.setSize(sf::Vector2f(idle.getSize().x, idle.getSize().y));
+		body.setTexture(&idle);
+		body.setOrigin(body.getSize().x/2, body.getSize().y/2);	
+		body.setScale(3, 3);	
+	}
+
+	bool colision(sf::RectangleShape &col){
+		sf::FloatRect b(body.getPosition().x, body.getPosition().y, body.getSize().x/2, body.getSize().y/2);
+		return b.intersects(col.getGlobalBounds());
+	}
+	float getSpeed(){ return speed;}
+};
+
+class Pipe{
+	sf::Texture up, down;
+	sf::RectangleShape U, D;
+public:
+	Pipe(){}
+	void setTexture(sf::Texture &u, sf::Texture &d){
+		up = u;
+		down = d;
+		U.setSize(sf::Vector2f(up.getSize()));
+		D.setSize(sf::Vector2f(down.getSize()));
+		U.setTexture(&up);	D.setTexture(&down);
+		U.setScale(3, 3);	D.setScale(3, 3);
+		U.setOrigin(U.getSize().x/2, 0-70);	D.setOrigin(D.getSize().x/2, D.getSize().y);
+		// D.setOrigin(D.getSize().x/2, 0-70);
+	}
+	void setPos(int x, int y){
+		U.setPosition(x, y);
+		D.setPosition(x, y);	
+	}
+	void draw(sf::RenderWindow &win){
+		win.draw(U);
+		win.draw(D);
+	}
+	sf::RectangleShape& getU(){ return U;}
+	sf::RectangleShape& getD(){ return D;}
+};
